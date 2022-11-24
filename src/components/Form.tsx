871e81efafa2fe3b53537e935ec1754001bb8c92
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-11-24 11:41:49 -0500

    Style list entry form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -26,37 +26,43 @@ const Form = ({
 					setItems(prev => randomSort(prev));
 					setComparing(true);
 				}}>
-				<fieldset>
+				<fieldset className="border border-slate-800 p-4 pt-2 mb-4">
 					<legend>Type of Sort</legend>
-					<input
-						id="top-sort"
-						type="radio"
-						name="sort-type"
-						value="top"
-						checked={sortType === 'top'}
-						onChange={() => setSortType('top')}
-					/>
-					<label htmlFor="top-sort">Top</label>
-					<input
-						id="all-sort"
-						type="radio"
-						name="sort-type"
-						value="all"
-						checked={sortType === 'all'}
-						onChange={() => setSortType('all')}
-					/>
-					<label htmlFor="all-sort">All</label>
+					<div className="grid gap-8 grid-cols-2 justify-items-center">
+						<div>
+							<input
+								id="top-sort"
+								type="radio"
+								name="sort-type"
+								value="top"
+								checked={sortType === 'top'}
+								onChange={() => setSortType('top')}
+							/>
+							<label htmlFor="top-sort">Top</label>
+						</div>
+						<div>
+							<input
+								id="all-sort"
+								type="radio"
+								name="sort-type"
+								value="all"
+								checked={sortType === 'all'}
+								onChange={() => setSortType('all')}
+							/>
+							<label htmlFor="all-sort">All</label>
+						</div>
+					</div>
 				</fieldset>
-				<label htmlFor="text-input" className="block">
+				<label htmlFor="text-input" className="block mb-2">
 					Enter Items, One Per Line
 				</label>
 				<textarea
-					className="block w-full h-48 mb-8"
+					className="block border border-slate-800 p-2 w-full h-48 mb-8"
 					id="text-input"
 					onChange={e => setItems(createItemsList(e.target.value))}
 				/>
 				<button
-					className="border border-slate-800 disabled:opacity-50"
+					className="border border-slate-800 disabled:opacity-50 py-2 px-4 w-full"
 					type="submit"
 					disabled={items.length < 2}>
 					Start
